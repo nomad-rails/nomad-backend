11f541004ea96aa1ff59933520173bd2f5698cbf
Ozodimgba <idasiadiachi@gmail.com>
2024-03-24 09:41:24 +0000

    main.ts cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.enableCors({
+    origin: '*',
+    methods: 'GET, HEAD,PUT,PATCH,POST,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
